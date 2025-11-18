ebc08aafdafbda369e45341b372610ac0c2e21d8
NilashishC <nchakrab@redhat.com>
2025-11-18 11:08:10 +0530

    fix catalog-info path and README
    Signed-off-by: NilashishC <nchakrab@redhat.com>

--- plugins/scaffolder-backend-module-backstage-rhaap/src/actions/createEEDefinition.ts
+++ plugins/scaffolder-backend-module-backstage-rhaap/src/actions/createEEDefinition.ts
@@ -418,7 +418,10 @@ export function createEEDefinitionAction(options: {
           // generate catalog descriptor file path for the Execution Environment
           // this is only needed if the user has chosen to publish to a SCM repository
           // and we are creating a catalog-info.yaml file using the built-in `catalog:write` action
-          const catalogInfoPath = path.join(eeDir, 'catalog-info.yaml');
+          const catalogInfoPath = path.join(
+            contextDirName,
+            'catalog-info.yaml',
+          );
           ctx.output('catalogInfoPath', catalogInfoPath);
         } else {
           // dynamically register the execution environment entity in the catalog
@@ -568,23 +571,27 @@ function generateReadme(
   const packages = values.systemPackages || [];
   const mcpServers = values.mcpServers || [];
 
-  return `# Ansible Execution Environment Definition
+  return `# Ansible Execution Environment Definition: Getting Started Guide
+
+Congratulations on Your New Execution Environment Definition file!
+
+This file tells **Ansible Builder** (the tool used to build EEs) how to create the build instruction file (\`Containerfile\` for **Podman**, \`Dockerfile\` for **Docker**) and the build context for your Execution Environment image.
+
+An EE is a container image (like a "workspace") that bundles all the tools and collections your automation needs to run consistently everywhere.
+This guide will walk you through the entire process, from reviewing your new files to using your final image in Ansible Automation Platform (AAP).
+
+## Review What Was Generated
+
+First, let us review the files that were just created for you:
+
+- \`${values.eeFileName}.yaml\` - This is the main definition file that \`ansible-builder\` will use to construct your EE image.
 
 ${
   publishToSCM
-    ? 'This directory contains the definition file for an Ansible Execution Environment.'
-    : ''
-}
-
-${publishToSCM ? `## Files Generated` : '## Files available for download'}
-
-- \`${values.eeFileName}.yaml\` - The Execution Environment definition file.
-${
-  publishToSCM
-    ? `- \`template.yaml\` - The software template for Ansible Portal that allows reusing this Execution Environment definition file to create custom ones.
-- \`catalog-info.yaml\` - The Catalog Entity Descriptor file that allows registering this Execution Environment as a catalog component in Ansible Portal.
+    ? `- \`template.yaml\` - This is the Ansible portal template file that generated this. You can use it as a base to create new templates for your portal.
+- \`catalog-info.yaml\` - This is the Ansible portal file that registers this Execution Environment definition as a "component" in your portal's catalog.
 `
-    : '- `README.md` - contains instructions on how to build and use the Execution Environment.'
+    : ''
 }
 
 ## Configuration
@@ -630,36 +637,51 @@ ${packages.map(pkg => `- \`${pkg}\``).join('\n')}
 
 `
       : ''
-  }## Next Steps: Build and Use the Execution Environment
-
-### Step 1: Install Required Tools
-
-To build this Execution Environment, you need to have the following tools installed:
-
-1. [ansible-builder](https://docs.redhat.com/en/documentation/red_hat_ansible_automation_platform/2.6/html/creating_and_using_execution_environments/assembly-using-builder)
-2. Container Engine: [Podman](https://podman.io/getting-started/installation) (recommended) or [Docker](https://docs.docker.com/engine/install/)
-
-### Step 2: Build the Execution Environment
-
+  }## Get the Execution Environment definition file
 ${
   publishToSCM
-    ? `1. Clone the repository and navigate to this directory.
+    ? `#### Clone the repository and navigate to this directory.
 
 \`\`\`bash
 git clone <repository URL>
-cd ${{ repositoryName }}/${{ contextDirName }}
+cd ${repositoryName}/${contextDirName}
 \`\`\`
+
 `
-    : `1. Download the Execution Environment definition file.`
-}
+    : `#### Download the Execution Environment definition file
 
-2. Customize the Execution Environment definition file as needed.
+1. Click on the "Download" button to download the Execution Environment tar file.
+2. Extract the Execution Environment definition file from the downloaded tar.
 
-\`\`\`\n
+\`\`\`bash
+tar -xvf ${contextDirName}.tar
+\`\`\`
 
-If one or more collections specified in your Execution Environment definition are to be pulled from Automation Hub (or a custom Galaxy server),
-please ensure that those servers are configured in a \`ansible.cfg\` file and included in the EE build.
-You can refer to this documentation for more details: [Configure Red Hat automation hub as the primary content source](https://docs.redhat.com/en/documentation/red_hat_ansible_automation_platform/2.4/html/getting_started_with_automation_hub/configure-hub-primary#proc-configure-automation-hub-server-cli)
+`
+}Open \`${values.eeFileName}.yaml\` in your favorite text editor and review the configuration.
+
+## Build Your Execution Environment (EE)
+
+### Step 1: Install Required Tools
+
+With your configuration ready, you'll need the following tools on your local machine to build the image:
+
+- ansible-builder (The tool that builds the EE).
+- A container engine: Podman (recommended) or Docker.
+
+If you don't have them, you can find installation guides here:
+
+1. [Installing ansible-builder](https://docs.redhat.com/en/documentation/red_hat_ansible_automation_platform/2.6/html/creating_and_using_execution_environments/assembly-using-builder)
+2. [Installing Podman](https://podman.io/getting-started/installation) (recommended) OR [Installing Docker](https://docs.docker.com/engine/install/)
+
+### Step 2 (Optional): Add additional configuration to the Execution Environment definition file
+
+Before you build the EE, you must ensure \`ansible-builder\` can access all the collections you listed in your \`${values.eeFileName}.yaml\` file.
+If the EE definition only uses collections from [Ansible Galaxy](https://galaxy.ansible.com/), you can safely **skip this step**.
+
+If one or more collections specified in your EE definition are to be pulled from Automation Hub (or a custom Galaxy server),
+please ensure that those servers are configured in an \`ansible.cfg\` file and included in the EE build.
+You can refer to this documentation for more details: [Configure Red Hat Automation Hub as the primary content source](https://docs.redhat.com/en/documentation/red_hat_ansible_automation_platform/2.4/html/getting_started_with_automation_hub/configure-hub-primary#proc-configure-automation-hub-server-cli)
 
 For reference, here is an example of an \`ansible.cfg\` file that includes the Red Hat Automation Hub server:
 
@@ -673,7 +695,7 @@ auth_url=https://sso.redhat.com/auth/realms/redhat-external/protocol/openid-conn
 token=<SuperSecretToken>
 \`\`\`
 
-To include an \`ansible.cfg\` file in your execution environment build specifying additional configuration such as Automation Hub settings, please add the following sections to the generated Execution Environment definition file:
+Next, include the \`ansible.cfg\` file in your Execution Environment build by adding the following sections to the generated EE definition file:
 
 \`\`\`yaml
 additional_build_files:
@@ -685,13 +707,18 @@ additional_build_steps:
     - COPY _build/configs/ansible.cfg /etc/ansible/ansible.cfg
 \`\`\`
 
-3. Run the following command to build the Execution Environment:
+### Step 3: Build Your Execution Environment
+
+Now you're ready to build! Open a terminal in the directory where the EE definition file exists and run the build command:
+
 \`\`\`bash
 ansible-builder build --file ${values.eeFileName}.yaml --tag ${values.eeFileName}:latest --container-runtime podman
 \`\`\`
 
-Please update the tag (specified after the \`--tag\` flag) to the desired tag for the built image and
-the container runtime (specified after the \`--container-runtime\` flag) to the installed container runtime.
+This command uses your \`${values.eeFileName}.yaml\` file to build an image and tags it as \`${values.eeFileName}:latest\`
+
+- You can update the tag (specified after the \`--tag\` flag) to the desired tag for the built image.
+- If you are using \`Docker\`, change the runtime with \`--container-runtime docker\`.
 
 The \`ansible-builder\` CLI supports passing build-time arguments to the container runtime,
 use the \`--build-arg\` flag to pass these arguments.
@@ -699,8 +726,9 @@ use the \`--build-arg\` flag to pass these arguments.
 For the full list of supported flags, refer to the
 [ansible-builder reference for \`build arguments\`](https://docs.redhat.com/en/documentation/red_hat_ansible_automation_platform/2.6/html/creating_and_using_execution_environments/assembly-using-builder)
 
+### Step 4 (Recommended): Test Your Execution Environment Locally
 
-### Step 3: Using the Execution Environment locally
+This is the best way to verify your EE works before you share it. To do this, you can use \`ansible-navigator\`.
 
 1. Install [\`ansible-navigator\`](https://docs.redhat.com/en/documentation/red_hat_ansible_automation_platform/2.6/html/using_content_navigator/assembly-intro-navigator_ansible-navigator):
 
@@ -717,24 +745,39 @@ Please refer to the following documentations for more details:
 ansible-navigator run playbook.yml --execution-environment-image ${values.eeFileName}:latest
 \`\`\`
 
-### Step 4: Using the Execution Environment with Ansible Automation Platform
+If it runs successfully, your EE is working!
 
-1. The built Execution Environment image must be pushed to a container registry.
+### Step 5: Push Your Execution Environment to a Container Registry
 
-2. Tag your local image for the target registry (e.g., \`quay.io/your-username/${values.eeFileName}:latest\`).
+To use this EE in Ansible Automation Platform (AAP), it must exist in a container registry (like quay.io, or [private Automation Hub](https://docs.redhat.com/en/documentation/red_hat_ansible_automation_platform/2.6/html/managing_automation_content/managing-containers-hub)).
+
+1. Tag your local image for the target registry (e.g., \`quay.io/your-username/${values.eeFileName}:latest\`).
 
 \`\`\`bash
 podman tag ${values.eeFileName}:latest quay.io/your-username/${values.eeFileName}:latest
 \`\`\`
 
-3. Push the tagged image to the target registry:
+2. Push the tagged image to the target registry:
 
 \`\`\`bash
 podman push quay.io/your-username/${values.eeFileName}:latest
 \`\`\`
 
-4. Create a new Execution Environment in Ansible Automation Platform using the tagged image and add it to a job template.
- See the [Add a execution environment to a job template](https://docs.redhat.com/en/documentation/red_hat_ansible_automation_platform/2.6/html/using_automation_execution/assembly-controller-execution-environments) documentation for more details.
+### Step 6: Use Your Execution Environment in Ansible Automation Platform (AAP)
+
+You're all set! Now just tell AAP about your new EE.
+
+1. In your AAP dashboard, navigate to Administration -> Execution Environments.
+2. Click the *Add* button.
+3. Fill in the fields:
+  - **Name**: A friendly name (e.g., "My New Custom EE")
+  - **Image**: The full path from Step 4 (e.g., quay.io/your-username/ee-new:latest)
+  - **Pull**: Choose your pull policy (e.g., "Always pull image before running")
+4. Click the *Save* button.
+
+Your new EE can now be selected in any Job Template.
+
+See the [Add a execution environment to a job template](https://docs.redhat.com/en/documentation/red_hat_ansible_automation_platform/2.6/html/using_automation_execution/assembly-controller-execution-environments) documentation for more details.
 `;
 }
 
